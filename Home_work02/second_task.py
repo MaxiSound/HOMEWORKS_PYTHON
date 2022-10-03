935a2e6e54fee1e56d0b953f12259a3f73e9c746
MaxiSound <maxisound@yandex.ru>
2022-10-03 14:04:44 +0700

    ready

--- Home_work02/second_task.py
+++ Home_work02/second_task.py
@@ -1,5 +1,6 @@
 # 2. Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 
+# 1 Вариант:
 n = int(input('Введите число: \n'))
 list = [1]
 
@@ -7,3 +8,12 @@ for i in range(1, n):
     list.append((i+1) * list[i-1])
 
 print(list)
+
+# 2 Вариант:
+# n = int(input('Введите число: \n'))
+# temp_num = 1
+# for i in range(n):
+#     temp_num *= i+1
+#     print(temp_num, end=', ')
+
+# print()

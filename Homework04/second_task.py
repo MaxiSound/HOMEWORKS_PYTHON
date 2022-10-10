6991572e95aa2d51d6c702c9865b6058d713e3a2
MaxiSound <maxisound@yandex.ru>
2022-10-10 15:53:09 +0700

    ready

--- Homework04/second_task.py
+++ Homework04/second_task.py
@@ -1,2 +1,19 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
-# Пример: N = 20 => [2, 2, 5]
+
+def number_factorization(N):
+    factors = []
+    divider = 2
+
+    while divider <= N:
+        if not N % divider:
+            factors.append(divider)
+            N /= divider
+        else:
+            divider += 1
+    print('Результат: ', factors)
+
+
+if __name__ == '__main__':
+    N = int(input("Введите число: \n"))
+    # print ('Результат: ', number_factorization(N))
+    number_factorization(N)

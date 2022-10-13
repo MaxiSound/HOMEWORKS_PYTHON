d331ba075e7134c0b03515d104a7d7d518a84d00
MaxiSound <maxisound@yandex.ru>
2022-10-13 21:13:42 +0700

    Так больше нравится

--- Homework03/fourth_task.py
+++ Homework03/fourth_task.py
@@ -1,12 +1,12 @@
 # Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 # Примеры: 45 -> 101101, 3 -> 11, 2 -> 10
 
-import os
-from random import *
-os.system('cls')
+input_num = int(input("Введите число:\n"))
 
-n = randint(1, 101)
-print('Задано число:\n', n)
+def Convertation (number):
+    if int(number) >= 2:
+        return str(Convertation(int(number/2))) + str(number % 2)
+    if int(number) == 1:
+        return '1'
 
-print('Двоичный вид:\n', "{0:b}".format(n))
-print('Двоичный вид:\n', bin(n)[2:])
+print ('Получим:\n', Convertation(input_num))
